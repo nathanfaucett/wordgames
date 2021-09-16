0e6d3aaa6865511873f987dc5f3350a6628c8368
Nathan Faucett <nathanfaucett@gmail.com>
2021-09-16 08:18:42 -0400

    remove appDir

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,6 @@ const config = {
 		adapter: adapter({
 			fallback: 'index.html'
 		}),
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'wordgames',
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#app'
 	}
