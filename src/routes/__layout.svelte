230988653eb9b77cd817de4038dccffbcf43cde7
Nathan Faucett <nathanfaucett@gmail.com>
2021-10-16 11:56:20 -0400

    fix icons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,10 @@
 <script context="module">
+	import { base } from '$app/paths';
 	import '../app.postcss';
 </script>
+
+<svelte:head>
+	<link rel="icon" href={`${base}/favicon.png`} />
+</svelte:head>
 
 <slot />
