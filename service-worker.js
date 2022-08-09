ec8be4fd58f4ab7a27b159b9339b67e9d6f2bef3
nathanfaucett <nathanfaucett@users.noreply.github.com>
2022-08-09 23:04:28 +0000

    deploy: 652c4f5d31d013c37188d62b7ef33f7d06f42880

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,20 @@
-const timestamp = 1636564321438;
-const build = [
-  "/wordgames/_app/start-39e8dfb0.js",
-  "/wordgames/_app/assets/start-464e9d0a.css",
-  "/wordgames/_app/pages/__layout.svelte-ad5917be.js",
-  "/wordgames/_app/assets/pages/__layout.svelte-5454e9cb.css",
-  "/wordgames/_app/error.svelte-93bf3d85.js",
-  "/wordgames/_app/pages/index.svelte-1f4e7472.js",
-  "/wordgames/_app/pages/lobby.svelte-7d1191b2.js",
-  "/wordgames/_app/pages/game.svelte-3de66c3d.js",
-  "/wordgames/_app/chunks/vendor-2d4221cb.js",
-  "/wordgames/_app/chunks/singletons-12a22614.js",
-  "/wordgames/_app/chunks/paths-28a87002.js",
-  "/wordgames/_app/chunks/preload-helper-ac1c8acd.js",
-  "/wordgames/_app/chunks/util-f1ec02fc.js",
-  "/wordgames/_app/chunks/userId-a5a0ac90.js",
-  "/wordgames/_app/chunks/easy-d1c5d374.js",
-  "/wordgames/_app/chunks/medium-73eb433f.js",
-  "/wordgames/_app/chunks/hard-1ca555bc.js"
-];
-const files = [
+const l = [
+  "/wordgames/internal/immutable/start-ff2c0377.js",
+  "/wordgames/internal/immutable/pages/__layout.svelte-3cdd55db.js",
+  "/wordgames/internal/immutable/assets/__layout-abec3685.css",
+  "/wordgames/internal/immutable/error.svelte-8201d305.js",
+  "/wordgames/internal/immutable/pages/game.svelte-a85c1ccc.js",
+  "/wordgames/internal/immutable/pages/index.svelte-ada279ab.js",
+  "/wordgames/internal/immutable/pages/lobby.svelte-5e666eba.js",
+  "/wordgames/internal/immutable/chunks/index-275a10fd.js",
+  "/wordgames/internal/immutable/chunks/singletons-80e721ca.js",
+  "/wordgames/internal/immutable/chunks/preload-helper-362d4722.js",
+  "/wordgames/internal/immutable/chunks/util-dfedccda.js",
+  "/wordgames/internal/immutable/chunks/userId-791e378d.js",
+  "/wordgames/internal/immutable/chunks/easy-d6952d4b.js",
+  "/wordgames/internal/immutable/chunks/medium-27fe57e1.js",
+  "/wordgames/internal/immutable/chunks/hard-6fdf5c11.js"
+], m = [
   "/wordgames/favicon.png",
   "/wordgames/icon.png",
   "/wordgames/manifest.json",
@@ -27,31 +23,40 @@ const files = [
   "/wordgames/sounds/alarm-clock.wav",
   "/wordgames/sounds/ticking.mp3",
   "/wordgames/sounds/ticking.wav"
-];
-const CACHE_NAME = `static-cache-v${timestamp}`;
-const FILES_TO_CACHE = [...build, ...files];
-self.addEventListener("install", (evt) => {
-  console.log("[ServiceWorker] Install");
-  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-    console.log("[ServiceWorker] Pre-caching offline page");
-    return cache.addAll(FILES_TO_CACHE);
-  }));
-  self.skipWaiting();
+], c = "1660086262055", n = self, o = `cache${c}`, r = l.concat(m), d = new Set(r);
+n.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(o).then((s) => s.addAll(r)).then(() => {
+      n.skipWaiting();
+    })
+  );
 });
-self.addEventListener("activate", (evt) => {
-  console.log("[ServiceWorker] Activate");
-  evt.waitUntil(caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
-    if (key !== CACHE_NAME) {
-      console.log("[ServiceWorker] Removing old cache", key);
-      return caches.delete(key);
-    }
-  }))));
-  self.clients.claim();
+n.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(async (s) => {
+      for (const a of s)
+        a !== o && await caches.delete(a);
+      n.clients.claim();
+    })
+  );
 });
-self.addEventListener("fetch", (evt) => {
-  console.log("[ServiceWorker] Fetch", evt.request.url);
-  if (evt.request.mode !== "navigate") {
-    return;
+async function u(e) {
+  const s = await caches.open(`offline${c}`);
+  try {
+    const a = await fetch(e);
+    return s.put(e, a.clone()), a;
+  } catch (a) {
+    const t = await s.match(e);
+    if (t)
+      return t;
+    throw a;
   }
-  evt.respondWith(fetch(evt.request).catch(() => caches.open(CACHE_NAME).then((cache) => cache.match("offline.html"))));
+}
+n.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET" || e.request.headers.has("range"))
+    return;
+  const s = new URL(e.request.url), a = s.hostname === self.location.hostname && s.port !== self.location.port, t = s.host === self.location.host && d.has(s.pathname), i = e.request.cache === "only-if-cached" && !t;
+  !a && !i && e.respondWith(
+    (async () => t && await caches.match(e.request) || u(e.request))()
+  );
 });
